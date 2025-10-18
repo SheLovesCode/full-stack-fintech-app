1096e1e031fab08ccae6949eed337463aab13cfe
DianaMun <diana@nsuna.com>
2025-10-18 14:32:59 +0200

    Create payout model

---
+++ backend/app/models/payout_model.py
@@ -0,0 +1,30 @@
+# app/models/user_model.py
+import enum
+from datetime import datetime
+from sqlalchemy import Integer, Column, Float, String, DateTime
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+import db
+
+
+class PayoutStatus(enum.Enum):
+    PENDING = "PENDING"
+    APPROVED = "APPROVED"
+    REJECTED = "REJECTED"
+
+
+class Payout(Base):
+    __tablename__ = "payouts"
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float, nullable=False)
+    date = Column(DateTime, default=datetime, nullable=False)
+    currency = Column(String, nullable=False)
+    status = Column(enum.Enum(PayoutStatus), nullable=False)
+    user_id = Column(Integer, foreign_key="user.id", nullable=False)
+    # description = Column(String(255), nullable=False)
+    owner=relationship("User", backref="payouts")
+
+
+
